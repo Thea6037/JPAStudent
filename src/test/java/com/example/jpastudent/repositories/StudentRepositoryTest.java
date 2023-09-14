ef9a374c86b5de929609adeb43f5799d1f92e473
Thea6037 <thea6037@stud.kea.dk>
2023-09-14 14:56:06 +0200

    test fix

--- src/test/java/com/example/jpastudent/repositories/StudentRepositoryTest.java
+++ src/test/java/com/example/jpastudent/repositories/StudentRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.jpastudent.repositories;
 import com.example.jpastudent.model.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
@@ -11,7 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@DataJpaTest
 class StudentRepositoryTest
 {
     @Autowired
@@ -36,12 +37,12 @@ class StudentRepositoryTest
         List<Student> lst = studentRepository.findAllByName("Tim");
 
         assertEquals(1, lst.size());
-    }*/
+    }
 
     @Test
     void testOneStudent(){
         List<Student> lst = studentRepository.findAll();
         assertEquals(1, lst.size());
-    }
+    }*/
 
 }
